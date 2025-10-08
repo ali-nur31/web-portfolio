d0e77f2d26f54d4eb67363cacd03d2d2a30fc863
Ali-Nur <alinursagynbaev31@gmail.com>
2025-10-08 08:41:31 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function openMenu() {
+  document.getElementById("fullMenu").classList.add("active");
+}
+
+function closeMenu() {
+  document.getElementById("fullMenu").classList.remove("active");
+}
